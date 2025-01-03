6c21a38863b061f6c2791741ef25e49f96ac363d
Aleksey-Kon <windowsviser@gmail.com>
2025-01-03 14:39:21 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# electro-swing-radio
+ 
---
+++ electro-swing-radio/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+namespace electro_swing_radio
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
